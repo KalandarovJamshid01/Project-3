08163b15ca9627d54e9d0181b0f96c0556262bae
Suhrob <atavullayevsuhrob@gmail.com.com>
2024-06-13 18:31:25 +0500

    okay, init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
-Drop Table IF EXISTS connections;
+-- Drop and recreate the 'connections' table
+DROP TABLE IF EXISTS connections;
 
 CREATE TABLE connections (
     id SERIAL PRIMARY KEY,
@@ -11,14 +12,16 @@ CREATE TABLE connections (
     img_id INT,
     created_at TIMESTAMP
 );
-INSERT INTO connections (id, mail, title, short_description, long_description, contact, img_url,img_id created_at) VALUES
-(14, 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'http://res.cloudinary.com/dq5dvbrtp/image/upload/v1718122567/my_website_images/nmfppijzzpcb73fcuxou.jpg',1,'2024-06-11T16:23:10.070Z'),
-(15, 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'http://res.cloudinary.com/dq5dvbrtp/image/upload/v1718122988/my_website_images/hlmliacvwol0t45jwcru.jpg',2,'2024-06-11T16:31:56.772Z'),
-(16, 'asdf', 'asdf', 'adsf', 'asdf', 'asdf', 'https://res.cloudinary.com/dq5dvbrtp/image/upload/v1718123773/my_website_images/n7845sndo9qtcgiuud8w.jpg',3, '2024-06-11T16:36:14.767Z'),
-(17, 'asdf', 'asdf', 'adsf', 'asdf', 'asdf', 'https://res.cloudinary.com/dq5dvbrtp/image/upload/v1718123808/my_website_images/o7lmfdkyhrgxctznfxl7.jpg',4, '2024-06-11T16:36:49.945Z');
 
+-- Corrected INSERT statement for 'connections' table
+INSERT INTO connections (id, mail, title, short_description, long_description, contact, img_url, img_id, created_at) VALUES
+(14, 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'http://res.cloudinary.com/dq5dvbrtp/image/upload/v1718122567/my_website_images/nmfppijzzpcb73fcuxou.jpg', 1, '2024-06-11T16:23:10.070Z'),
+(15, 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'http://res.cloudinary.com/dq5dvbrtp/image/upload/v1718122988/my_website_images/hlmliacvwol0t45jwcru.jpg', 2, '2024-06-11T16:31:56.772Z'),
+(16, 'asdf', 'asdf', 'adsf', 'asdf', 'asdf', 'https://res.cloudinary.com/dq5dvbrtp/image/upload/v1718123773/my_website_images/n7845sndo9qtcgiuud8w.jpg', 3, '2024-06-11T16:36:14.767Z'),
+(17, 'asdf', 'asdf', 'adsf', 'asdf', 'asdf', 'https://res.cloudinary.com/dq5dvbrtp/image/upload/v1718123808/my_website_images/o7lmfdkyhrgxctznfxl7.jpg', 4, '2024-06-11T16:36:49.945Z');
 
-Drop Table IF EXISTS images;
+-- Drop and recreate the 'images' table
+DROP TABLE IF EXISTS images;
 
 CREATE TABLE images (
     id SERIAL PRIMARY KEY,
@@ -34,6 +37,7 @@ CREATE TABLE images (
     created_at TIMESTAMP
 );
 
+-- Corrected INSERT statement for 'images' table
 INSERT INTO images (id, img_url, title, context, file_name, width, height, format, folder, bytes, created_at) VALUES 
 (48, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718167314/my_website_images/axfsfvbuqbanysgpjrkl.jpg', 'dfg', 'dfg', 'file', 1700, 2185, 'jpg', 'my_website_images', 143403, '2024-06-12T04:41:55.116Z'),
 (47, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718167291/my_website_images/pvsrfwsp4si6bb3ozfvb.jpg', 'qwe', 'qwe', 'file', 1948, 2505, 'jpg', 'my_website_images', 165162, '2024-06-12T04:41:32.151Z'),
@@ -50,7 +54,4 @@ INSERT INTO images (id, img_url, title, context, file_name, width, height, forma
 (36, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166646/my_website_images/ry4isijzo3r3x0s3ucf3.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:46.922Z'),
 (35, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166646/my_website_images/oypgzvzevmg4w0ifv06f.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:46.897Z'),
 (34, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166645/my_website_images/m8ox0ekg12l44v4e7ahc.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:46.092Z'),
-(32, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166645/my_website_images/bvswye39tgjipxh7vfpm.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:46.022Z'),
-(31, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166645/my_website_images/rthhnpbgvdtfeyetszie.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:45.987Z'),
-(30, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166645/my_website_images/msedkjqhs4qegpnvi0o6.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:45.983Z'),
-(28, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166063/my_website_images/jkxnq5atiojycbshl7pe.jpg', '123', '123', 'file', 1700, 2186, 'jpg', 'my_website_images', 149243, '2024-06-12T04:21:04.136Z);
+(32, 'http://res.cloudinary.com/dl8bppbqg/image/upload/v1718166645/my_website_images/bvswye39tgjipxh7vfpm.png', '32', '32', 'file', 32, 32, 'png', 'my_website_images', 975, '2024-06-12T04:30:46.022Z');
